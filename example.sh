e7d2faf88564d0aa807b77e95cfb4409d0239c9f
Sam Blake <samuel.thomas.blake@gmail.com>
2024-11-21 10:50:48 +1100

    Update example.sh
    Removed old options.

--- example.sh
+++ example.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-./quagmire3 -cipher cipher.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -keywordlen 7 -cyclewordlen 7 -nsigmathreshold 1. -nhillclimbs 2500 -nrestarts 15000 -backtrackprob 0.15 -verbose
-
+./quagmire -cipher cipher.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -keywordlen 7 -cyclewordlen 7 -nhillclimbs 2500 -nrestarts 15000 -backtrackprob 0.15 -verbose
 
