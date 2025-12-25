a36baf208f02634c58af4ad6e24ca024209241cf
Sam Blake <samuel.thomas.blake@gmail.com>
2025-12-25 19:31:18 +1100

    Add files via upload

---
+++ ciphers/tests/example_vigenere.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./quagmire -type 0 -cipher cipher_vigenere.txt -ngramsize 5 -ngramfile english_quintgrams.txt -nhillclimbs 500 -nrestarts 100 -backtrackprob 0.15 -slipprob 0.0005 -verbose
