39f9862dd8b8e0d1a786507c060ea57e6e1a3301
Sam Blake <samuel.thomas.blake@gmail.com>
2026-02-24 12:26:34 +1100

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./quagmire -cipher cipher.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -keywordlen 7 -cyclewordlen 7 -nhillclimbs 2500 -nrestarts 15000 -backtrackprob 0.15 -verbose
+./polyalphabetic -type quag3 -cipher cipher.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -keywordlen 7 -cyclewordlen 7 -nhillclimbs 500 -nrestarts 1000 -backtrackprob 0.15 -verbose
 
