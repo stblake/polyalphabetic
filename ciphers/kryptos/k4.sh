4b1496032a98c03bd5828f49fd346ced29edeeeb
Sam Blake <samuel.thomas.blake@gmail.com>
2026-02-24 12:32:14 +1100

    Update k4.sh

--- ciphers/kryptos/k4.sh
+++ ciphers/kryptos/k4.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
-./quagmire -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 100000 -backtrackprob 0.15 -verbose
+./polyalphabetic -type vig -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 10000 -backtrackprob 0.15 -verbose
+
+./polyalphabetic -type beaufort -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 10000 -backtrackprob 0.15 -verbose
+
+./polyalphabetic -type porta -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 10000 -backtrackprob 0.15 -verbose
+
+./polyalphabetic -type quag1 -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 10000 -backtrackprob 0.15 -verbose
+
+./polyalphabetic -type quag2 -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 10000 -backtrackprob 0.15 -verbose
+
+./polyalphabetic -type quag3 -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 10000 -backtrackprob 0.15 -verbose
+
+./polyalphabetic -type quag4 -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 10000 -backtrackprob 0.15 -verbose
+
+# variants
+
+./polyalphabetic -type vig -variant -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 10000 -backtrackprob 0.15 -verbose
+
+./polyalphabetic -type beaufort -variant -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 10000 -backtrackprob 0.15 -verbose
+
+./polyalphabetic -type porta -variant -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 10000 -backtrackprob 0.15 -verbose
+
+./polyalphabetic -type quag1 -variant -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 10000 -backtrackprob 0.15 -verbose
+
+./polyalphabetic -type quag2 -variant -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 10000 -backtrackprob 0.15 -verbose
+
+./polyalphabetic -type quag3 -variant -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 10000 -backtrackprob 0.15 -verbose
+
+./polyalphabetic -type quag4 -variant -cipher k4.txt -crib crib.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 10000 -backtrackprob 0.15 -verbose
+
+
+# autokey
+
+
+
 
 
