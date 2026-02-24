6aa3b7812f982d70aed26bd7a84782bb08209271
Sam Blake <samuel.thomas.blake@gmail.com>
2026-02-24 12:29:28 +1100

    Update k2.sh

--- ciphers/kryptos/k2.sh
+++ ciphers/kryptos/k2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./quagmire -cipher k2.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 100000 -backtrackprob 0.15 -verbose
+./polyalphabetic -type quag3 -cipher k2.txt -ngramsize 4 -ngramfile english_quadgrams.txt -nhillclimbs 500 -nrestarts 100000 -backtrackprob 0.15 -verbose
 
 
